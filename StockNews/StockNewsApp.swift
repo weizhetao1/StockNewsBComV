b8df83f38213a41ce803df580c120a477a056409
Matthew Collins <m.collins@etoncollege.org.uk>
2023-03-22 08:24:23 +0000

    Initial Commit

---
+++ StockNews/StockNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockNewsApp.swift
+//  StockNews
+//
+//  Created by Collins, Matthew - MC on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StockNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
